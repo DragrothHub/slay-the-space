7c77ceaa1958ba80509a73aaf0c2a8e66318c2a3
DragrothHub <jugger-hell@gmx.net>
2026-08-27 23:03:04 +0200

    Simplyfied processModules

--- src/engine/processModules.js
+++ src/engine/processModules.js
@@ -1,153 +1,63 @@
 import { getFriendlyUnits } from "./helpers";
+import { moduleCollection } from "../data/modules";
+
+function getModuleCount(ship, effect) {
+    return ship.modules.filter(
+        moduleId => moduleCollection[moduleId]?.effect === effect
+    ).length;
+}
 
 export function processTurnStartModules(activeShip, battleState){
 
-    // kinetic boost
-    if(activeShip.modules.includes("module_kineticboost_shield")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_kineticboost_shield"
-        ).length;
-
-        activeShip.attributes.kineticAtk += 100 * count;
+    // kineticboost
+    const kineticboostCount = getModuleCount(activeShip, "kineticboost");
+    if(kineticboostCount > 0){
+        activeShip.attributes.kineticAtk += 100 * kineticboostCount;
     }
 
-    if(activeShip.modules.includes("module_kineticboost_armor")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_kineticboost_armor"
-        ).length;
-
-        activeShip.attributes.kineticAtk += 100 * count;
-    }
-
-    if(activeShip.modules.includes("module_kineticboost_mixed")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_kineticboost_mixed"
-        ).length;
-
-        activeShip.attributes.kineticAtk += 100 * count;
-    }
-
-    // laser boost
-    if(activeShip.modules.includes("module_laserboost_shield")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_laserboost_shield"
-        ).length;
-
-        activeShip.attributes.kineticAtk += 100 * count;
-    }
-
-    if(activeShip.modules.includes("module_laserboost_armor")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_laserboost_armor"
-        ).length;
-
-        activeShip.attributes.kineticAtk += 100 * count;
-    }
-
-    if(activeShip.modules.includes("module_laserboost_mixed")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_laserboost_mixed"
-        ).length;
-
-        activeShip.attributes.kineticAtk += 100 * count;
+    // laserboost
+    const laserboostCount = getModuleCount(activeShip, "laserboost");
+    if(laserboostCount > 0){
+        activeShip.attributes.laserAtk += 100 * laserboostCount;
     }
 }
 
 export function processTurnEndModules(activeShip, battleState){
 
-    // repair bot
-    if(activeShip.modules.includes("module_repairbot_shield")){
-
-        const repairbotCount = activeShip.modules.filter(
-            id => id === "module_repairbot_shield"
-        ).length;
-
-        activeShip.stats.currentShield += 5 * repairbotCount;
-        battleState.log.push(`${activeShip.name} was repaired by repair bot (+${5 * repairbotCount} Shield).`);
+    // kineticboost
+    const kineticboostCount = getModuleCount(activeShip, "kineticboost");
+    if(kineticboostCount > 0){
+        activeShip.attributes.kineticAtk -= 100 * kineticboostCount;
     }
 
-    if(activeShip.modules.includes("module_repairbot_armor")){
-
-        const repairbotCount = activeShip.modules.filter(
-            id => id === "module_repairbot_armor"
-        ).length;
-
-        activeShip.stats.currentArmor += 5 * repairbotCount;
-        battleState.log.push(`${activeShip.name} was repaired by repair bot (+${5 * repairbotCount} Armor).`);
+    // laserboost
+    const laserboostCount = getModuleCount(activeShip, "laserboost");
+    if(laserboostCount > 0){
+        activeShip.attributes.laserAtk -= 100 * laserboostCount;
     }
 
-    if(activeShip.modules.includes("module_repairbot_mixed")){
-
-        const repairbotCount = activeShip.modules.filter(
-            id => id === "module_repairbot_mixed"
-        ).length;
-
-        activeShip.stats.currentShield += 3 * repairbotCount;
-        activeShip.stats.currentArmor  += 2 * repairbotCount;
-        battleState.log.push(`${activeShip.name} was repaired by repair bot (+${3 * repairbotCount} Shield / +${2 * repairbotCount} Armor).`);
+    //repairbot_shield
+    const repairbotShieldCount = getModuleCount(activeShip, "repairbot_shield");
+    if(repairbotShieldCount > 0){
+        activeShip.stats.currentShield += 5 * repairbotShieldCount;
+        battleState.log.push(`${activeShip.name} was repaired by repair bot (+${5 * repairbotShieldCount} Shield).`);
     }
 
-    // kinetic boost
-    if(activeShip.modules.includes("module_kineticboost_shield")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_kineticboost_shield"
-        ).length;
-
-        activeShip.attributes.kineticAtk -= 100 * count;
+    //repairbot_armor
+    const repairbotArmorCount = getModuleCount(activeShip, "repairbot_armor");
+    if(repairbotArmorCount > 0){
+        activeShip.stats.currentArmor += 5 * repairbotArmorCount;
+        battleState.log.push(`${activeShip.name} was repaired by repair bot (+${5 * repairbotArmorCount} Armor).`);
     }
 
-    if(activeShip.modules.includes("module_kineticboost_armor")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_kineticboost_armor"
-        ).length;
-
-        activeShip.attributes.kineticAtk -= 100 * count;
+    //repairbot_mixed
+    const repairbotMixedCount = getModuleCount(activeShip, "repairbot_mixed");
+    if(repairbotMixedCount > 0){
+        activeShip.stats.currentShield += 3 * repairbotMixedCount;
+        activeShip.stats.currentArmor  += 2 * repairbotMixedCount;
+        battleState.log.push(`${activeShip.name} was repaired by repair bot (+${3 * repairbotMixedCount} Shield / +${2 * repairbotMixedCount} Armor).`);
     }
 
-    if(activeShip.modules.includes("module_kineticboost_mixed")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_kineticboost_mixed"
-        ).length;
-
-        activeShip.attributes.kineticAtk -= 100 * count;
-    }
-
-    // laser boost
-    if(activeShip.modules.includes("module_laserboost_shield")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_laserboost_shield"
-        ).length;
-
-        activeShip.attributes.kineticAtk -= 100 * count;
-    }
-
-    if(activeShip.modules.includes("module_laserboost_armor")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_laserboost_armor"
-        ).length;
-
-        activeShip.attributes.kineticAtk -= 100 * count;
-    }
-
-    if(activeShip.modules.includes("module_laserboost_mixed")){
-
-        const count = activeShip.modules.filter(
-            id => id === "module_laserboost_mixed"
-        ).length;
-
-        activeShip.attributes.kineticAtk -= 100 * count;
-    }
 }
 
 export function processOutgoingDamageModules(
@@ -158,11 +68,10 @@ export function processOutgoingDamageModules(
     battleState
 ) {
 
-    if (activeShip.modules.includes("module_formation_shield") || activeShip.modules.includes("module_formation_armor") || activeShip.modules.includes("module_formation_mixed")) {
-
+    if(getModuleCount(activeShip, "formation") > 0){
         const formationCount = getFriendlyUnits(battleState, activeShip).filter(
-            ship => ship.modules.includes("module_formation_shield") || ship.modules.includes("module_formation_armor") || ship.modules.includes("module_formation_mixed")
-        ).length;
+            ship => getModuleCount(ship, "formation") > 0
+        ).length - 1;
 
         damage *= 1 + (0.10 * formationCount);
 
@@ -181,11 +90,10 @@ export function processIncomingDamageModules(
     battleState
 ) {
 
-    if (target.modules.includes("module_formation_shield") || target.modules.includes("module_formation_armor") || target.modules.includes("module_formation_mixed")) {
-
+    if(getModuleCount(target, "formation") > 0){
         const formationCount = getFriendlyUnits(battleState, target).filter(
-            ship => ship.modules.includes("module_formation_shield") || ship.modules.includes("module_formation_armor") || ship.modules.includes("module_formation_mixed")
-        ).length;
+            ship => getModuleCount(ship, "formation") > 0
+        ).length - 1;
 
         damage *= 1 - (0.05 * formationCount);
 
