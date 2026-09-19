e578e297b5bcc9497d56ca2e06e3e81925620cf3
DragrothHub <jugger-hell@gmx.net>
2026-09-19 16:28:20 +0200

    Fixing layout break on detonatons due to long log messages

--- src/components/BattleLog.jsx
+++ src/components/BattleLog.jsx
@@ -4,13 +4,28 @@ export default function BattleLog() {
 
     const { gameState } = useGameState();
 
-    if(!gameState.run?.battle?.log) return null;
+    if (!gameState.run?.battle?.log) return null;
 
     return (
-        <div style={{ marginTop: 20, marginBottom: 100, }}>
+        <div
+            style={{
+                marginTop: 20,
+                marginBottom: 100,
+                minWidth: 0,
+            }}
+        >
             <h3 style={{ color: "rgb(158, 203, 255)" }}>Log</h3>
+
             {gameState.run.battle.log.slice(-20).reverse().map((entry, i) => (
-                <div key={i} style={{ fontSize: "0.8em" }}>{entry}</div>
+                <div
+                    key={i}
+                    style={{
+                        fontSize: "0.8em",
+                        overflowWrap: "anywhere",
+                    }}
+                >
+                    {entry}
+                </div>
             ))}
         </div>
     );
